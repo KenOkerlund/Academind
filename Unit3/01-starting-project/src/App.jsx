15508bfad313ac0a1005771cdb09375ab2b101b6
Ken Okerlund <kokerlund@wasatch.utah.gov>
2024-06-10 14:49:29 -0600

    Dynamically displaying and styling content for the examples section

--- Unit3/01-starting-project/src/App.jsx
+++ Unit3/01-starting-project/src/App.jsx
@@ -3,14 +3,15 @@ import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button')
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton)
-    console.log(selectedButton)
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
   }
 
   return (
@@ -34,12 +35,27 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
