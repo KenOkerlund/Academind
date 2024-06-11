e0790fc410c3fb0c92a8caca3e83f9321cd13c0a
Ken <ken.okerlund@gmail.com>
2024-06-11 17:19:37 -0600

    Player names and game board created

---
+++ Unit4/02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export default function Player({ initialName, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(e){
+        setPlayerName(e.target.value)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+    // let btnCaption = 'Edit'
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}></input>;
+        // btnCaption = 'Save'
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : "Edit"}</button>
+        </li>
+    )
+}
