e5bd28122f991f2d281b963209d9b25daab815bc
Ken Okerlund <ken.okerlund@gmail.com>
2025-04-23 14:32:12 -0600

    Restructure fetch calls for greater flexibility.

--- Unit15/Sending-Http-Requests/src/components/AvailablePlaces.jsx
+++ Unit15/Sending-Http-Requests/src/components/AvailablePlaces.jsx
@@ -4,6 +4,7 @@ import ErrorPage from "./ErrorPage.jsx";
 import Places from "./Places.jsx";
 
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -25,16 +26,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
