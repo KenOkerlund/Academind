8309ac4e8482417c4dcc690ddc0f91234b49e190
Ken Okerlund <ken.okerlund@gmail.com>
2025-04-23 12:47:40 -0600

    Added functionality to filter based on location

--- Unit15/Sending-Http-Requests/src/components/AvailablePlaces.jsx
+++ Unit15/Sending-Http-Requests/src/components/AvailablePlaces.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import ErrorPage from "./ErrorPage.jsx";
 import Places from "./Places.jsx";
 
+import { sortPlacesByDistance } from "../loc.js";
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -30,14 +32,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message: error.message || "An error occurred. Please try again later",
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
